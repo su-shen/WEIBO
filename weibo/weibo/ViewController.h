2b0eca5290da8dcbf78090d2707ee6d29e52c08f
xiaomage <xiaomage@qq.com>
2016-05-19 20:52:37 +0800

    初始化操作

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by sy on 16/5/19.
+//  Copyright © 2016年 sy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
